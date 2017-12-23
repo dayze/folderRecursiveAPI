3d7abb960d43bd131237ca6aa86988dd66a9a54c
Dez <elye24@gmail.com>
2017-12-23 20:40:46 +0100

    add of cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors')
 const app = express()
 const {readRecursive} = require('./utils')
 
+app.use(cors())
 app.use(helmet())
 
 app.get('/getFiles', (req, res) => {
