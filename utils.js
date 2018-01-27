a6993fd7e8b181adeb3ac5ef99919a9fec63c73b
Dez <elye24@gmail.com>
2018-01-27 01:19:04 +0100

    change algorithm files

--- utils.js
+++ utils.js
@@ -1,19 +1,20 @@
 const fs = require('fs')
 const {exec} = require('child_process')
+const uniqid = require('uniqid')
 module.exports.readRecursive = (path) => {
-  let list = []
+  let list = {}
   let files = fs.readdirSync(path)
   for (let file of files) {
     let stats = fs.lstatSync(`${path}/${file}`)
     if (stats.isDirectory()) {
-      list.push({
+      list[uniqid()] = {
         name: file,
         path: `${path}/${file}`,
         type: 'directory',
         files: this.readRecursive(`${path}/${file}`)
-      })
+      }
     } else {
-      list.push({name: file, path: `${path}/${file}`, type: 'file'})
+      list[uniqid()] = {name: file, path: `${path}/${file}`, type: 'file'}
     }
   }
   return list
